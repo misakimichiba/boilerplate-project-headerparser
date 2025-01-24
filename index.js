baac8729427c7e7cd4a1e37dd58c51e5552688ae
Misaki Michiba <misakimichiba@gmail.com>
2025-01-24 04:12:15 +0000

    installed ip
    JSON object of ip address is sort of returned but will be improved on

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var os = require('os');
+var ip = require('ip');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -22,6 +24,12 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// request that returns IP, preferred language and software of user
+app.get('/api/whoami', function (req, res) {
+  var networkInterfaces = os.networkInterfaces();
+  res.json({ ipaddress: networkInterfaces['eth0'][0]['address'] });
 });
 
 // listen for requests :)
